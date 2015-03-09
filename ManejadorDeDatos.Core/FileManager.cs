1f3f89dec84e7221b1a6353124a4c054dc97da73
aztekpride <aztekpridec@gmail.com>
2015-03-09 12:19:57 -0600

    Se agrego funcion para agregar archivos y renombrarlos

--- ManejadorDeDatos.Core/FileManager.cs
+++ ManejadorDeDatos.Core/FileManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,21 @@ namespace ManejadorDeDatos.Core
 {
     public class FileManager
     {
+        public static void CrearArchivo(string nombreArchivo)
+        {
+
+            var BD = File.CreateText(nombreArchivo + ".txt");
+            BD.Close();
+
+
+        }
+
+
+        public static void reNombrar(string nombreArchivo, string nuevoNombre)
+        {
+
+            File.Move(nombreArchivo+"txt", nuevoNombre+"txt");
+
+        }
     }
 }
