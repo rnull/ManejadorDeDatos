0a36de31a6ff8bf3c599115735029db69e66f73e
aztekpride <aztekpridec@gmail.com>
2015-05-25 11:18:58 -0500

    Se agrego algoritmos de ordenamiento de burbuja

---
+++ Algoritmos/MetodoDeOrdenamiento.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmos
+{
+    public class MetodoDeOrdenamiento
+    {
+
+        //Metodo burbuja 
+        public static void mBurbuja(int[] caracteres)
+        {
+
+            int[] equivalenciASCII = new int[caracteres.Length];
+
+            for (int i = 0; i < caracteres.Length; i++)
+            {
+                equivalenciASCII[i] = (int)caracteres[i];
+            }
+
+            Aplicar(equivalenciASCII);
+
+            for (int i = 0; i < caracteres.Length; i++)
+            {
+                caracteres[i] = (char)equivalenciASCII[i];
+
+            }
+
+        }
+
+        private static void Aplicar(int[] numeros)
+        {
+
+            for (int i = 1; i < numeros.Length; i++)
+            {
+                for (int j = 0; j < numeros.Length - 1; j++)
+                {
+                    if (numeros[j] > numeros[j + 1])
+                    {
+                        int auxiliar = numeros[j];
+                        numeros[j] = numeros[j + 1];
+                        numeros[j + 1] = auxiliar;
+                    }
+                }
+            }
+
+        }    
+
+        // Fin metodo burbuja
+
+
+    }
+}
