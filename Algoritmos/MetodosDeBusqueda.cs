148be764142c78a57db24d05773f1f64f9274d49
Edgar Manuel García González <manolo161070@hotmail.com>
2015-06-22 09:25:25 -0500

    Se agrego MetodosDeBusqueda/busquedaBinaria

--- Algoritmos/MetodosDeBusqueda.cs
+++ Algoritmos/MetodosDeBusqueda.cs
@@ -23,5 +23,18 @@ namespace Algoritmos
             return -1; //No se encontro coincidencia
         }
         //Fin BusquedaLineal
+        //Inicio Busqueda Binaria
+        public static int busquedaBinaria(List<int> vector, int superior, int dato)
+        {
+            int centro, inf = 0, sup = superior - 1;
+            while (inf <= sup)
+            {
+                centro = ((sup - inf) / 2) + inf;
+                if (vector[centro] == dato) return centro;
+                else if (dato < vector[centro]) sup = centro - 1;
+                else inf = centro + 1;
+            }
+            return -1;
+        }
     }
 }
